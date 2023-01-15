47306df2d18481567d8a5f16a67ec03371bb2387
Julia <julia.rogovaja@gmail.com>
2023-01-15 13:38:48 +0200

    hw4

---
+++ hw4_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов, 
+//  и выводит их на экран. (числа берете любые)
+
+int[] newArray = GetArray(8);
+System.Console.WriteLine();
+System.Console.WriteLine($"[{String.Join(',', newArray)}]");
+
+//function
+
+int[] GetArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(9);
+    }
+    return arr;
+}
